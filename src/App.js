2338460fe05a43a321bab7eb0a263623b06e3cb6
Predrag <pedja.antic97@gmail.com>
2020-10-24 22:04:05 +0200

    Adding dependencies and importing webcam

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, {useRef} from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import * as posenet from "@tensorflow-models/posenet";
+import Webcam from "react-webcam"
+
+
 function App() {
+const webcamRef = useRef(null);
+const canvasRef = useRef(null);
+
+const runPosenet = async () => {
+  const net = await posenet.load({
+    inputResolution: {width:640, height:480},
+    scale:0.5
+  });
+
+  setInterval(() => {
+    detect(net)
+  }, 100);
+}
+
+const detect = async (net) => {
+  if(typeof webcamRef.current!=="undefined" && webcamRef.current !== null && webcamRef.current.video.readyState===4) {
+    const video = webcamRef.current.video;
+
+    // Get Video Properties
+    const videoWidth = webcamRef.current.video.videoWidth;
+    const videoHeight = webcamRef.current.video.videoHeight;
+
+    // Set video width
+    webcamRef.current.video.width = videoWidth;
+    webcamRef.current.video.height = videoHeight;
+
+    // Make Detections
+    const pose = await net.estimateSinglePose(video);
+    console.log(pose);
+  }
+}
+
+runPosenet();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam mirrored="true" 
+          ref={webcamRef}
+          style={{
+          position: "absolute", 
+          marginLeft: "auto", 
+          marginRight:"auto", 
+          left:0, 
+          right:0, 
+          textAlign:"center", 
+          zIndex:9,
+          width: 640,
+          height:480}} />
+
+        <canvas mirrored="true"
+          ref={canvasRef}
+          style={{
+          position: "absolute",
+          marginLeft: "auto", 
+          marginRight:"auto", 
+          left:0, 
+          right:0, 
+          textAlign:"center", 
+          zIndex:9,
+          width: 640, 
+          height:480}}/>
       </header>
     </div>
   );
