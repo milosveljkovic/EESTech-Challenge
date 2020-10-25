21e7ed854769971c87a2bc73843dd5fce606e16d
zbalanovic <z.balanovic@elfak.rs>
2020-10-25 08:37:20 +0100

    paper recognition and ratio calculating

--- Flask/main.py
+++ Flask/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, make_response
+import cv2
 import utils
 
 # runner microservice starts on port 5000
@@ -8,11 +9,12 @@ app = Flask(__name__)
 @app.route('/api/process_img', methods=['POST'])
 def process_img():
     data = request.json
-    img = data['ImageSrc']
+    img = data['ImgSrc']
     left_shoulder = data['leftShoulder']
     right_shoulder = data['rightShoulder']
     left_hip = data['leftHip']
     right_hip = data['rightHip']
+
     ratio = utils.process_img(img)
     response = {'shoulderWidth': utils.get_shoulder_width(ratio, left_shoulder, right_shoulder),
                 'hipWidth': utils.get_hip_width(ratio, left_hip, right_hip)
--- Flask/utils.py
+++ Flask/utils.py
@@ -1,21 +1,39 @@
 import math
-
+import cv2
+import numpy as np
+import imutils
 
 def get_shoulder_width(ratio, left_shoulder, right_shoulder):
     print(left_shoulder['x'])
-    len_squared = (left_shoulder['y'] - right_shoulder['y'])**2 + (left_shoulder['x'] - right_shoulder['x'])**2
+    len_squared = (left_shoulder['position']['y'] - right_shoulder['position']['y'])**2 + (left_shoulder['position']['x'] - right_shoulder['position']['x'])**2
     length = math.sqrt(len_squared)
     return length * ratio
 
 
 def get_hip_width(ratio, left_hip, right_hip):
-    len_squared = (left_hip['y'] - right_hip['y'])**2 + (left_hip['x'] - right_hip['x'])**2
+    len_squared = (left_hip['position']['y'] - right_hip['position']['y'])**2 + (left_hip['position']['x'] - right_hip['position']['x'])**2
     length = math.sqrt(len_squared)
     return length * ratio
 
 
 # NOT IMPLEMENTED
 def process_img(img):
-    ratio = 1
-
+    image = cv2.imread(img)
+    marker = find_marker(image)
+    ratio = 21/marker[1][0]
     return ratio
+
+
+def find_marker(image):
+    # convert the image to grayscale, blur it, and detect edges
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 35, 125)
+
+    # find the contours in the edged image and keep the largest one;
+    # we'll assume that this is our piece of paper in the image
+    (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    c = max(cnts, key = cv2.contourArea)
+    # compute the bounding box of the of the paper region and return it
+    return cv2.minAreaRect(c)
+
